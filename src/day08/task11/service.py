75a8176671d3ca915dabaed3898a2a0faa134250
yangjaeyeon05 <didwodus1234@gmail.com>
2024-08-23 15:08:13 +0900

    8/23 수정

--- src/day08/task11/service.py
+++ src/day08/task11/service.py
@@ -9,9 +9,18 @@ def samsungData():
     for row in rows[1: len(rows) - 1]:  # 첫 줄 제외한 행 반복
         print(row)
         cols = row.split(',')
-        dic = {'일자': eval(cols[0]),'종가': eval(cols[1]),'대비': eval(cols[2]),'등락률': eval(cols[3]),'시가': eval(cols[4]),
-               '고가': eval(cols[5]),'저가': eval(cols[6]),'거래량': eval(cols[7]),'거래대금': eval(cols[8]),
-               '시가총액': eval(cols[9]),'상장주식수': eval(cols[10])}
+        dic = {
+            '일자': eval(cols[0]),
+            '종가': format(int(eval(cols[1])) , ','),  # 천단위 쉼표 : format(숫자데이터 , ',')
+            '대비': eval(cols[2]),
+            '등락률': eval(cols[3]),
+            '시가': eval(cols[4]),
+            '고가': eval(cols[5]),
+            '저가': eval(cols[6]),
+            '거래량': eval(cols[7]),
+            '거래대금': eval(cols[8]),
+            '시가총액': eval(cols[9]),
+            '상장주식수': eval(cols[10])}
         print(dic)
         list.append(dic)
     return list
