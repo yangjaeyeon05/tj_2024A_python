6cfcc6bfb0002bf57467dc7874bf2babe93b5e0f
yangjaeyeon05 <didwodus1234@gmail.com>
2024-08-27 20:23:44 +0900

    8/27 쿠우쿠우

--- src/day10/5_쿠우쿠우매장.py
+++ src/day10/5_쿠우쿠우매장.py
@@ -9,6 +9,10 @@
 import urllib.request
 import pandas as pd
 from bs4 import BeautifulSoup
+import json
+from flask import Flask
+
+app = Flask(__name__)
 
 def qooqoo_store(result):
     for page in range(1 , 7):  # 1 ~ 50까지 반복
@@ -22,33 +26,47 @@ def qooqoo_store(result):
         tbody = soup.select('tbody')
         # print(tbody)
         for row in tbody[0].select("tr"):
-            # if len(row) == 3:
-            #     break
             # print(f'>>> row : {row}')
             # print('end')
             tds = row.select('td')
+            # print(len(tds))
+            if len(tds) < 5: continue
             # print(f'>>>tds : {tds}')
-            for i in tds[1]:
-                print(f'>>>i {i}')
-            # num = tds[0].string;    print(num)
-            # subject = tds[1].select_one(".td-subject").select("a")[1].string; # print(subject)
-            # contact = tds[2].select_one("a").string; # print(contact)
-            # address = tds[3].select('a')[0].string; # print(address)
-            # time = tds[4].select('a')[0].string; # print(time)
-            # store = [num , subject , contact , address , time]
-            #result.append(store)    # 리스트 안에 리스트 요소 추가 : 2차원 리스트 [ [] [] [] ]
+            num = tds[0].string.strip(' ').strip('\n');    # print(num)
+            subject = tds[1].select_one(".td-subject").select("a")[1].string.strip(' ').strip('\n').strip('\"'); # print(subject)
+            contact = tds[2].select_one("a").string.strip(' '); # print(contact)
+            address = tds[3].select('a')[0].string.strip(' ').strip('\"'); # print(address)
+            time = tds[4].select('a')[0].string.strip(' ').strip('\"'); # print(time)
+            store = [num , subject , contact , address , time]
+            result.append(store)    # 리스트 안에 리스트 요소 추가 : 2차원 리스트 [ [] [] [] ]
     return
 
 def main():
     result = []  # 할리스 매장 정보 리스트를 여러개 저장하는 리스트 변수 , 2차원 리스트
     print(">>> 쿠우쿠우 매장 크롤링 중 >>>")
     qooqoo_store(result)
-    print(result)  # 확인
-    # # py 리스트 객체를 DataFrame 객체로 변환
-    # hollys_tbl = pd.DataFrame(result, columns=('store', 'sido-gu', 'address', 'phone'))
-    # # DataFrame 객체를 csv 파일로 생성
-    # hollys_tbl.to_csv('hollys.csv', encoding='cp949', mode='w', index=False)
-    # del result[:]
+    # print(result)  # 확인
+    # py 리스트 객체를 DataFrame 객체로 변환
+    qooqoo_tbl = pd.DataFrame(result, columns=('num', 'subject', 'contact', 'address' , 'time'))
+    # DataFrame 객체를 csv 파일로 생성
+    qooqoo_tbl.to_csv('qooqoo.csv', encoding='utf-8', mode='w', index=False)
+    del result[:]
+
+@app.route("/qooqoo" , methods = ["GET"])
+def index():
+    main()
+    # csv파일 DataFrame 객체로 불러오기
+    data = pd.read_csv('qooqoo.csv', encoding='utf-8', index_col=0, engine='python')
+    # print(f'>>>data : {data}')
+    # DataFrame 객체 json으로 변환
+    df = pd.DataFrame(data)
+    df_json = df.to_json(orient='index', indent=4, force_ascii=False )
+    print(df_json)
+    # 파일 저장
+    with open(f'qooqoo.json', 'w', encoding='utf-8') as file:
+        jsonFile = json.dumps(df_json, indent=4, sort_keys=True, ensure_ascii=False)
+        file.write(jsonFile)
+    return df_json
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
